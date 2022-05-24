75ef34b850448f5e2461bba30cfdf3318106a2b2
edssadeq <essadeq0701@gmail.com>
2022-05-24 16:29:57 +0100

    adding quiz_student model

---
+++ models/student_quiz.model.js
@@ -0,0 +1,33 @@
+module.exports = (mongoose) => {
+  // function recives mongoose
+  const student_quiz = mongoose.model(
+    "student_quiz",
+    mongoose.Schema(
+      {
+        passedAt: {
+          type: Date,
+          required: [true, "Must set quiz passing date!"],
+        },
+        duration: {
+          type: Number,
+          require: false, // just to not be hard to deal with
+        },
+        scorp: {
+          type: Number,
+          require: [true, "A quiz passed? without score? seriously?"],
+        },
+        student: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "student",
+        },
+        quiz: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "quiz",
+        },
+      },
+      { timestamps: true } // auto createdAt and updatedAt
+    )
+  );
+
+  return student_quiz;
+};
