9cd3617d5235e1b1b5d8dbad2ecefeca0335a917
edssadeq <essadeq0701@gmail.com>
2022-05-24 17:32:12 +0100

    basic controller done for studentQuizModel

--- controllers/student_quiz.controller.js
+++ controllers/student_quiz.controller.js
@@ -137,33 +137,35 @@ exports.update = (req, res) => {
 // Delete a Quiz with the specified id in the request
 exports.delete = (req, res) => {
   const id = req.params.id;
-  quizModel
+  studentQuizModel
     .findByIdAndRemove(id)
     .then((data) => {
       if (!data) {
         res.status(404).json({
-          message: `Cannot delete Quiz with id=${id}. Maybe it was not found!`,
+          message: `Cannot delete studentQuiz with id=${id}. Maybe it was not found!`,
         });
       } else {
         res.json({
-          message: "Quiz was deleted successfully!",
+          message: "studentQuiz was deleted successfully!",
         });
       }
     })
     .catch((err) => {
       res.status(500).json({
         message:
-          "Could not delete Quiz with id=" + id + ". Maybe it was not found!",
+          "Could not delete studentQuiz with id=" +
+          id +
+          ". Maybe it was not found!",
       });
     });
 };
 // Delete all Quizes from the database.
 exports.deleteAll = (req, res) => {
-  quizModel
+  studentQuizModel
     .deleteMany({})
     .then((data) => {
       res.json({
-        message: `${data.deletedCount} Quiz records were deleted successfully!`,
+        message: `${data.deletedCount} studentQuiz records were deleted successfully!`,
       });
     })
     .catch((err) => {
@@ -171,247 +173,5 @@ exports.deleteAll = (req, res) => {
         message:
           err.message || "Some error occurred while removing all records.",
       });
-    });
-};
-
-// Find all quiz Quizs
-exports.findQuizTeacher = (req, res) => {
-  const id = req.params.id;
-  quizModel
-    .findById(id)
-    .then((quiz) => {
-      if (!quiz)
-        res.status(404).json({ message: "No Quiz found with id " + id });
-      else {
-        // find teacher
-        db.teacherModel
-          .findById(quiz.teacher)
-          .then((teacher) => {
-            res.json(teacher);
-          })
-          .catch((err) => {
-            res.status(500).json({
-              message: "Error retrieving Teacher with id=" + quiz.teacher,
-            });
-          });
-        // add teacher
-        // res.json(data);
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({ message: "Error retrieving Quiz with id=" + id });
-    });
-};
-exports.findQuizStudents = (req, res) => {
-  const id = req.params.id;
-  quizModel
-    .findById(id)
-    .then((quiz) => {
-      if (!quiz)
-        res.status(404).json({ message: "No Quiz found with id " + id });
-      else {
-        // find teacher
-        db.studentModel
-          .find({
-            _id: { $in: quiz.students },
-          })
-          .then((students) => {
-            res.json(students);
-          })
-          .catch((err) => {
-            res.status(500).json({
-              message: err,
-            });
-          });
-        // add teacher
-        // res.json(data);
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({ message: "Error retrieving Quiz with id=" + id });
-    });
-};
-exports.findQuizQuestions = (req, res) => {
-  const id = req.params.id;
-  quizModel
-    .findById(id)
-    .then((quiz) => {
-      if (!quiz)
-        res.status(404).json({ message: "No Quiz found with id " + id });
-      else {
-        // find teacher
-        db.questionModel
-          .find({
-            _id: { $in: quiz.questions },
-          })
-          .then((questions) => {
-            res.json(questions);
-          })
-          .catch((err) => {
-            res.status(500).json({
-              message: err,
-            });
-          });
-        // add teacher
-        // res.json(data);
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({ message: "Error retrieving Quiz with id=" + id });
-    });
-};
-/*
-exports.addQuizQuestions = (req, res) => {
-  const id = req.params.id;
-  quizModel
-    .findById(id)
-    .then((quiz) => {
-      if (!quiz)
-        res.status(404).json({ message: "No Quiz found with id " + id });
-      else {
-        // find teacher
-        db.teacherModel
-          .findById(quiz.teacher)
-          .then((teacher) => {
-            res.json(teacher);
-          })
-          .catch((err) => {
-            res.status(500).json({
-              message: "Error retrieving Teacher with id=" + quiz.teacher,
-            });
-          });
-        // add teacher
-        // res.json(data);
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({ message: "Error retrieving Quiz with id=" + id });
-    });
-};*/
-exports.associateTeacherToQuiz = (req, res) => {
-  if (!req.body) {
-    return res.status(400).json({
-      message: "Data can not be empty!",
-    });
-  }
-  // TODO: __
-  // validate student
-  const id = req.params.id;
-  quizModel
-    .findByIdAndUpdate(
-      id,
-      { teacher: req.body.teacherID },
-      { useFindAndModify: false }
-    )
-    .then((data) => {
-      if (!data) {
-        res.status(404).json({
-          message: `Cannot update Quiz with id=${id}. Maybe Quiz was not found!`,
-        });
-      } else res.json({ message: "Quiz was updated successfully." });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        message:
-          "Error updating Quiz with id=" + id + ". Maybe Quiz was not found!",
-      });
-    });
-};
-exports.addStudentToQuiz = (req, res) => {
-  if (!req.body) {
-    return res.status(400).json({
-      message: "Data can not be empty!",
-    });
-  }
-  // TODO: __
-  // validate student
-  const id = req.params.id;
-  quizModel
-    .findByIdAndUpdate(
-      id,
-      { $addToSet: { students: req.body.studentID } },
-      { useFindAndModify: false }
-    )
-    .then((data) => {
-      if (!data) {
-        res.status(404).json({
-          message: `Cannot update Quiz with id=${id}. Maybe Quiz was not found!`,
-        });
-      } else res.json({ message: "Quiz was updated successfully." });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        message:
-          "Error updating Quiz with id=" + id + ". Maybe Quiz was not found!",
-      });
-    });
-};
-exports.addQuestionToQuiz = (req, res) => {
-  if (!req.body) {
-    return res.status(400).json({
-      message: "Data can not be empty!",
-    });
-  }
-  // TODO: __
-  // validate student
-  const id = req.params.id;
-  quizModel
-    .findByIdAndUpdate(
-      id,
-      { $addToSet: { questions: req.body.questionID } },
-      { useFindAndModify: false }
-    )
-    .then((data) => {
-      if (!data) {
-        res.status(404).json({
-          message: `Cannot update Quiz with id=${id}. Maybe Quiz was not found!`,
-        });
-      } else res.json({ message: "Quiz was updated successfully." });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        message:
-          "Error updating Quiz with id=" + id + ". Maybe Quiz was not found!",
-      });
-    });
-};
-
-exports.delteTeacherFromQuiz = (req, res) => {
-  const id = req.params.id;
-  quizModel
-    .findById(id)
-    .then((data) => {
-      if (!data)
-        res.status(404).json({ message: "No Quiz found with id " + id });
-      else res.json(data);
-    })
-    .catch((err) => {
-      res.status(500).json({ message: "Error retrieving Quiz with id=" + id });
-    });
-};
-exports.deleteStudentFromQuiz = (req, res) => {
-  const id = req.params.id;
-  quizModel
-    .findById(id)
-    .then((data) => {
-      if (!data)
-        res.status(404).json({ message: "No Quiz found with id " + id });
-      else res.json(data);
-    })
-    .catch((err) => {
-      res.status(500).json({ message: "Error retrieving Quiz with id=" + id });
-    });
-};
-exports.deleteQuestionFromQuiz = (req, res) => {
-  const id = req.params.id;
-  quizModel
-    .findById(id)
-    .then((data) => {
-      if (!data)
-        res.status(404).json({ message: "No Quiz found with id " + id });
-      else res.json(data);
-    })
-    .catch((err) => {
-      res.status(500).json({ message: "Error retrieving Quiz with id=" + id });
     });
 };
